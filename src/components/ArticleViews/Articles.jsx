ffddfacf0f22de78431259dc95deb15f0e6fc027
Tiffany Yang <teefyang@Tiffanys-MacBook-Pro.local>
2021-03-21 16:54:42 -0700

    cusomtizes table style

--- src/components/ArticleViews/Articles.jsx
+++ src/components/ArticleViews/Articles.jsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import InputGroup from 'react-bootstrap/InputGroup';
-import FormControl from 'react-bootstrap/FormControl';
-import Button from 'react-bootstrap/Button';
-import BootstrapTable from 'react-bootstrap-table-next';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
-
+import React from "react";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+import BootstrapTable from "react-bootstrap-table-next";
+import "bootstrap/dist/css/bootstrap.css";
+import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
+//
 export default function Articles(props) {
   const { articles } = props;
-  console.log('articles', articles);
   return (
     <>
       <InputGroup>
         <FormControl
-          placeholder='Any articles you find interesting'
-          aria-label='(ie. React hooks tutorial)'
-          aria-describedby='basic-addon2'
+          placeholder="Any articles you find interesting"
+          aria-label="(ie. React hooks tutorial)"
+          aria-describedby="basic-addon2"
         />
         <InputGroup.Append>
-          <Button variant='outline-secondary'>Save</Button>
+          <Button variant="outline-secondary">Save</Button>
         </InputGroup.Append>
       </InputGroup>
       <ArticleResults articles={articles} />
@@ -28,48 +27,44 @@ export default function Articles(props) {
 
 function ArticleResults(props) {
   const { articles } = props;
-  const uiView = { isBasicList: true };
   const columns = [
     {
-      dataField: 'id',
-      text: 'Article ID',
+      dataField: "id",
+      text: "Article ID",
     },
     {
-      dataField: 'title',
-      text: 'Article Name',
+      dataField: "title",
+      text: "Article Name",
     },
     {
-      dataField: 'tags',
-      text: 'Tags',
+      dataField: "tags",
+      text: "Tags",
     },
     {
-      dataField: 'link',
-      text: 'Links',
+      dataField: "link",
+      text: "Links",
     },
     {
-      dataField: 'projects',
-      text: 'Projects',
+      dataField: "projects",
+      text: "Projects",
     },
   ];
+  const rowStyle = {
+    border: "solid 2px #ccc",
+    overflowWrap: "break-word",
+  };
   // const { isPending } = useContext(ModuleUpdateContext);
   return (
     <>
-      {/* {!articles.length ? (
-        <div className='alert alert-warning' style={{ marginTop: '20px' }}>
-          No articles found
-        </div>
-      ) : null}
+      {!articles.length ? <div>No articles found</div> : null}
 
-      {isPending ? <Loading /> : null} */}
-
-      {/* {uiView.isGridView ? (
-        <GridView menuBarHeight={menuBarHeight} articles={articles} />
-      ) : uiView.isBasicList ? (
-        <BasicListView articles={articles} />
-      ) : uiView.isCoversList ? (
-        <CoversView articles={articles} />
-      ) : null} */}
-      <BootstrapTable keyField='id' data={articles} columns={columns} />
+      {/* {isPending ? <Loading /> : null} */}
+      <BootstrapTable
+        keyField="id"
+        data={articles}
+        columns={columns}
+        rowStyle={rowStyle}
+      />
     </>
   );
 }
