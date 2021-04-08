48392e8b7726733a2f11d506c554c43d76c554d2
Tiffany Yang <teefyang@Tiffanys-MacBook-Pro.local>
2021-04-08 01:19:24 -0700

    adds Modal

--- src/views/ArticleViews/Articles.jsx
+++ src/views/ArticleViews/Articles.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import BootstrapTable from "react-bootstrap-table-next";
 import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 import BasicTable from "./BasicTable.jsx";
 import ToolkitProvider, { Search } from "react-bootstrap-table2-toolkit";
 import {
@@ -17,6 +17,7 @@ export default function Articles(props) {
   const { articles, setArticles, useArticles } = props;
   const queryClient = useQueryClient();
   const { status, data, error, isFetching } = useArticles();
+  const [modalShow, setModalShow] = useState(false);
 
   const { SearchBar } = Search;
 
@@ -65,27 +66,36 @@ export default function Articles(props) {
               <h2>Search By Article Name:</h2>
               <SearchBar {...props.searchProps} />
               <hr />
+              <MyVerticallyCenteredModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+                modalBody={
+                  <Button
+                    onClick={(e) => {
+                      //use the coming new state for the modal edit
+                      e.preventDefault();
+                      addArticle([
+                        {
+                          id: 0,
+                          title:
+                            "How State Management works? Dead simple SM in Vanilla JavaScript",
+                          link:
+                            "https://dev.to/vijaypushkin/dead-simple-state-management-in-vanilla-javascript-24p0?utm_source=digest_mailer&utm_medium=email&utm_campaign=digest_email",
+                          tags: [],
+                          notes: [
+                            "https://github.com/tyang1/Github_Library",
+                            "https://github.com/tyang1/Prospect_Emails_OSHackathon",
+                          ],
+                        },
+                      ]);
+                    }}
+                  >
+                    Save
+                  </Button>
+                }
+              />
               <div style={{ marginBottom: "10px" }}>
-                <Button
-                  onClick={(e) => {
-                    //use the coming new state for the modal edit
-                    e.preventDefault();
-                    addArticle([
-                      {
-                        id: 0,
-                        title:
-                          "How State Management works? Dead simple SM in Vanilla JavaScript",
-                        link:
-                          "https://dev.to/vijaypushkin/dead-simple-state-management-in-vanilla-javascript-24p0?utm_source=digest_mailer&utm_medium=email&utm_campaign=digest_email",
-                        tags: [],
-                        notes: [
-                          "https://github.com/tyang1/Github_Library",
-                          "https://github.com/tyang1/Prospect_Emails_OSHackathon",
-                        ],
-                      },
-                    ]);
-                  }}
-                >
+                <Button variant="primary" onClick={() => setModalShow(true)}>
                   add more articles
                 </Button>
               </div>
@@ -95,5 +105,27 @@ export default function Articles(props) {
         </ToolkitProvider>
       )}
     </>
+  );
+}
+
+function MyVerticallyCenteredModal(props) {
+  const { modalBody } = props;
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Modal heading
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>{modalBody}</Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
   );
 }
